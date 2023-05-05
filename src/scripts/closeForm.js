3ce09127b7d5608c3ca06530e8d3c0f186f4ae30
muchcreative <robby.j.lem@hotmail.com>
2023-05-05 16:15:18 -0400

    Temproary code removal of removePopup.js file for test deployment

---
+++ src/scripts/closeForm.js
@@ -0,0 +1,15 @@
+export default function closeForm() {
+  return (
+    <>
+      <script type="text/javascript">
+        function closeForm() {
+          // const pwdForm = document.getElementById("pwd-form");
+          // const bgBlock = document.getElementById('bg-block');
+
+          // pwdForm.style.display = "none";
+          // bgBlock.style.display = "none";
+        }   
+      </script>
+    </>
+  )
+}
