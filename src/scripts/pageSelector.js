e05a1f52c2e4475e592c3c0b2e55c5529ec3a78c
muchcreative <robby.j.lem@hotmail.com>
2023-05-05 15:58:29 -0400

    Initial commit

---
+++ src/scripts/pageSelector.js
@@ -0,0 +1,24 @@
+export default function pageSelector() {
+  return (
+    <>
+      <script>
+        function pageSelector() {
+          
+        }
+      </script>
+    </>
+  )
+}
+
+// yeah ok if you reduce the screen sized
+// your kinda ruining the game yeah
+
+// you have to know how to name your JS files here
+
+// function openForm() {
+//   document.getElementById("myForm").style.display = "block";
+// }
+
+// function closeForm() {
+//   document.getElementById("myForm").style.display = "none";
+// }
