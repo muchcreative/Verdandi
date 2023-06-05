f71eb52a579d51d00546dd6ea58fc7506ae0aa85
muchcreative <robby.j.lem@hotmail.com>
2023-06-05 15:43:32 -0400

    fixed contact form submit button and added submit message

--- src/components/night/form.js
+++ src/components/night/form.js
@@ -1,52 +1,69 @@
+// Next.js
+import { useState } from 'react';
+
 // CSS
 import formStyles from 'src/styles/night/Form.module.css';
 
+// External Libs
+import { clsx } from 'clsx';
+
 // Form must send out a post request and email confirmation as well
-// rewrite onFocus or Active pseudo element styles for borders
+// rewrite onFocus or Active pseudo element styles for input borders
 // Look into autocomplete styles
 
 export default function Form() {
-  return (
-    <>
-      <form action='/api/form' id='contact-form' method='post'>
-        <div className={formStyles.formGrid}>
-          <div className={formStyles.nameArea}>
-            <label htmlFor='name' hidden>Name</label>
-            <input
-              id='name'
-              className={formStyles.nameInput}
-              type='text'
-              name='name'
-              placeholder='Name'
-              required />
+    const [submitted, setSubmitted] = useState(false);
+
+    return (
+      <>
+        <form
+          id='contact-form'
+          action='/api/form'
+          onSubmit={() => setSubmitted(true)}
+          method='post'>
+            <div className={formStyles.formGrid}>
+                <div className={formStyles.nameArea}>
+                  <label htmlFor='name' hidden>Name</label>
+                <input
+                  id='name'
+                  className={formStyles.nameInput}
+                  type='text'
+                  name='name'
+                  placeholder='Name'
+                  required />
+            </div>
+            <div className={formStyles.emailArea}>
+                <label htmlFor='email' hidden>Email</label>
+                <input
+                  id='email'
+                  className={formStyles.emailInput}
+                  type='email'
+                  name='email'
+                  placeholder='Email'
+                  required />
+            </div>
+            <div className={formStyles.messageArea}>
+                <label htmlFor='message' hidden>Message</label>
+                <textarea
+                  id='message'
+                  className={formStyles.messageInput}
+                  name='message'
+                  form='contact-form'
+                  wrap='off'
+                  placeholder='Message'
+                  required>
+                </textarea>
+            </div>
+            <div className={formStyles.buttonArea}>
+                <button className={formStyles.submitButton} type='submit'>Send</button>
+            </div>
           </div>
-          <div className={formStyles.emailArea}>
-            <label htmlFor='email' hidden>Email</label>
-            <input
-              id='email'
-              className={formStyles.emailInput}
-              type='email'
-              name='email'
-              placeholder='Email'
-              required />
+          <div className={clsx({
+            [formStyles.showSubmitMessage] : submitted,
+            [formStyles.hideSubmitMessage] : !submitted})}>
+              <p>Thank you for your message. I will get back to you as soon as I can.</p>
           </div>
-          <div className={formStyles.messageArea}>
-            <label htmlFor='message' hidden>Message</label>
-            <textarea
-              id='message'
-              className={formStyles.messageInput}
-              name='message'
-              form='contact-form'
-              wrap='off'
-              placeholder='Message'
-              required>
-            </textarea>
-          </div>
-          <div className={formStyles.buttonArea}>
-            <button className={formStyles.button} type='submit'>Send</button>
-          </div>
-        </div>
-      </form>
-    </>
-  )
+        </form>
+      </>
+    )
 }
