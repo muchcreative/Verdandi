1d9c8b595311dbfc52fdbd085c630343cfd9d621
muchcreative <robby.j.lem@hotmail.com>
2023-07-20 15:40:23 -0400

    Changed SVG text divider to hr element

--- src/components/day/day.js
+++ src/components/day/day.js
@@ -13,10 +13,8 @@ const LiveClock = dynamic(() => import('src/components/day/live_clock.js'), {ssr
 
 // SVGs
 import MainDivider from 'public/day/main_divider.svg';
-import TextDivider from 'public/day/text_divider.svg';
 
 const mainDivider = () => <MainDivider />; 
-const textDidiver = () => <TextDivider />;
 
 const allura = Allura({
                 subsets: ['latin'], 
@@ -44,9 +42,9 @@ export default function Day() {
             />
             <div className={`${allura.className} ${styles.rightContent}`}>
               <h1 className={styles.line1}>I&#39;m shy before I eat my lunch.</h1>
-              <TextDivider className={`${svgStyles.textDivider} ${svgStyles.topTextDivider}`} />
+              <hr className={`${styles.textDivider} ${styles.topTextDivider}`} />
               <LiveClock />
-              <TextDivider className={`${svgStyles.textDivider} ${svgStyles.botTextDivider}`} />
+              <hr className={`${styles.textDivider} ${styles.botTextDivider}`} />
               <p className={styles.line2}>Can you come back past noon, <br className={styles.mobileBr}></br><em className={styles.highlight}>after 3pm?</em></p>
             </div>
           </div>
