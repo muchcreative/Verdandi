286eec7343f063f9041fbc163810eb9374e53b70
muchcreative <robby.j.lem@hotmail.com>
2023-07-26 12:57:44 -0400

    Change over to night page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ function setDayNight() {
     // Returns Day Page if daytime and Night Page if nighttime.
     const currentHour = format(new Date(), 'H');
     return (currentHour >= 1 && currentHour < 15 ?
-        <Day /> : <Night />);
+        <Night /> : <Night />);
 }
 
 export default function Home() {
