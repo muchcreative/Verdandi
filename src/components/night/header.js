47f1703b7f60e478f6052588735446b2eac7cb1b
muchcreative <robby.j.lem@hotmail.com>
2023-09-13 12:53:22 -0400

    Fixed header bug where background would flicker

--- src/components/night/header.js
+++ src/components/night/header.js
@@ -12,98 +12,84 @@ import { clsx } from 'clsx';
 
 const transLine = () => <TransLine />;
 
-export default function Main() {
+export default function Header() {
+    /*  
+    Component controls the background transitions and animations
+    when you scroll to the end of the scroll line. Note the '#header' 
+    div controls the starting and final background color
+    The animations are as follows when you scroll to the 
+    header section:
+
+    1. setVisbility(true)
+      - A fixed overlying div with class '.animations' becomes visible
+      - Animations are then triggered for their respective classes
+    2. Transition line will draw itself in
+    3. A sequence of background changes closely follow the transition 
+       line as a moving background image
+    4. The '.animations' div then sets "visiblity: hidden"
+       to reveal the actual header that has been faded in 
+       with the new background color.
+    */
+
     const [visible, setVisibility] = useState(false);
 
     useEffect(() => {
-      // Set transition line position
+      // Sets transition line starting position to match end position of scroll line
       const root = document.querySelector(':root');
       const svgElement = document.querySelector('#desk-line');
       root.style.setProperty('--trans-line-pos', svgElement.getBoundingClientRect().right + 'px');
     }, [])
 
     useEffect(() => {
-      const whiteSpace = document.querySelector('#float');
-      const bgChange1 = document.querySelector('#bg-change-1');
-      const bgChange2 = document.querySelector('#bg-change-2');
-      const bgChange3 = document.querySelector('#bg-change-3');
-  
       const preStart = 50;
       const afterEnd = 50;
-      const whiteSpaceLoc = whiteSpace.getBoundingClientRect();
 
+      const content = document.querySelector('#header-content');
+      const contentLoc = content.getBoundingClientRect();
+      
       const bgTransition = () => {
         // Scroll in range of the animation and scroll needs to freeze for the animation
-        if (whiteSpaceLoc.top - preStart <= window.scrollY && window.scrollY <= whiteSpaceLoc.bottom + afterEnd) {
+        if (window.scrollY >= contentLoc.top - preStart) {
             setVisibility(true);
-            bgChange1.style.backgroundPosition = 'top';   
-            bgChange2.style.backgroundPosition = 'bottom';    
-            bgChange3.style.backgroundPosition = 'top';
 
-            const scrollTop = window.scrollY || document.documentElement.scrollTop;
-            const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
-
-            if (screen.width >= 1000) {
-              window.onscroll = function() {
-                window.scrollTo(scrollLeft, scrollTop + 150);
-              };
-            } else {
-              window.onscroll = function() {
-                window.scrollTo(scrollLeft, scrollTop + 60);
-              };
-            }
+            // Freeze user's scroll location for 1-2 seconds so they don't continue scrolling down
+            window.onscroll = function() {
+              window.scrollTo(0, contentLoc.top);
+            };
 
             setTimeout(() => {
               window.onscroll = function() {};
             }, 1600);
-            
+
             document.removeEventListener('scroll', bgTransition);
-
-        // Scroll out of range of animation and scroll should not freeze for the animation
-        } else if (window.scrollY >= whiteSpaceLoc.bottom + afterEnd) {
+            
+        } else if (window.scrollY >= contentLoc.bottom + afterEnd) {
+            // Scroll out of range of animation and scroll should not freeze for the animation  
             setVisibility(true);
-            bgChange1.style.backgroundPosition = 'top';   
-            bgChange2.style.backgroundPosition = 'bottom';    
-            bgChange3.style.backgroundPosition = 'top';
-
-        // At beginning of page, nothing happens
-        } else {
-            bgChange3.style.backgroundPosition = 'bottom';
-            bgChange2.style.backgroundPosition = 'top';
-            bgChange1.style.backgroundPosition = 'bottom';
-            setVisibility(false);
+            document.removeEventListener('scroll', bgTransition);
         }
       }
-
-      document.addEventListener('scroll', bgTransition);
+    document.addEventListener('scroll', bgTransition);  
     }, [])
 
     return (
       <>
-        <div id='float' className={clsx({
-          [headerStyles.visibleFloat] : visible,
-          [headerStyles.hideFloat] : !visible})}>
-            <div id='bg-change-1' className={headerStyles.bgChange1}>
-                <div className={headerStyles.verticalContainer}>
-                    <TransLine className={clsx({
-                      [headerStyles.transitionLine] : true,
-                      [headerStyles.animateLine] : visible})} />
-                </div>
-            </div>
-            <div id='bg-change-2' className={clsx({
-              [headerStyles.bgChange2] : true,
-              [headerStyles.bg2Visible] : visible})}></div>
-            <div id='bg-change-3' className={clsx({
-              [headerStyles.bgChange3] : true,
-              [headerStyles.bg3Visible] : visible})}></div>
-        </div>
+        {/* Animations, TransLine Draw-In + BG Changes */}
         <div className={clsx({
+          [headerStyles.animations]: true,
+          [headerStyles.animateContainer]: visible})}>
+            <TransLine className={clsx({
+              [headerStyles.transLine] : true,
+              [headerStyles.animateLine] : visible})} />
+        </div>
+
+        {/* Header Apperance */}
+        <div id='header-content' className={clsx({
           [headerStyles.hookContainer] : true,
-          [headerStyles.showContainer] : visible, 
-          [headerStyles.hideContainer] :!visible})}>
-          <h1 id='hook' className={clsx({
+          [headerStyles.showHookContainer] : visible})}>
+          <h1 className={clsx({
             [headerStyles.hook] : true,
-            [headerStyles.hideHook] : !visible})}>
+            [headerStyles.showHook] : visible})}>
               Because <br className={headerStyles.mobileBr}></br>
               nobody <br className={headerStyles.mobileBr}></br>
               <br className={headerStyles.tabletBr}></br>
@@ -114,7 +100,7 @@ export default function Main() {
               same two <br className={headerStyles.mobileBr}></br>
               <br className={headerStyles.tabletBr}></br>
               <br className={headerStyles.deskBr}></br>
-              <em className={headerStyles.hookKey}>products.</em>
+              <em className={headerStyles.hookHighlight}>products.</em>
           </h1>
         </div>
       </>    
